2893f09e89b04412dbcb9106e4f186da196e66d0
Katie <kmurley@gmail.com>
2020-02-07 19:04:44 +0100

    .

--- Socket.c
+++ Socket.c
@@ -152,12 +152,13 @@ int func_read_message(int sockfd1)
     bzero(buff, MAX);
     len=read(sockfd1, buff, sizeof(buff));
     
-    //if(len=0){
-    //    printf("\n\nserver disconnected!!\n\n");
-    //    end_param=1;
-   // }
+
     // why is this not commented?
     printf("From server (%d)\n", len);
+     if(len=0){
+         printf("\n\nserver disconnected!!\n\n");
+         end_param=1;
+    }
     
     if(buff[0]!=0x99){
         //printf("    message header not recognized: %x\n",buff[0]);
